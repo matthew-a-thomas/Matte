4039d7781baf33e22205f0a525a1da68c94ebf46
Matt Thomas <me@matthewathomas.com>
2017-12-21 20:57:44 -0500

    Pared down gaussian elimination classes.
    Now calling GaussianEliminationHelpers.Solve on a list of coefficients will give you a list of steps to follow in order to put the corresponding solutions into reduced row echelon form.
    
    This should help speed up solution-finding since solution-finding no longer directly involves manipulating data, only coefficients.

---
+++ Matt.Math.Linear.Solving/GaussianEliminationHelpers.cs
@@ -0,0 +1,131 @@
+﻿namespace Matt.Math.Linear.Solving
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Solves a system of equations
+    /// </summary>
+    public sealed class GaussianEliminationHelpers
+    {
+        /// <summary>
+        /// Puts the given system of equations into reduced row echelon form as much as possible. Returns the steps
+        /// required to solve the system of equations. The steps will end with a <see cref="Operation.Complete"/> step
+        /// if the system of equations is solvable.
+        /// </summary>
+        /// <remarks>
+        /// The given <paramref name="coefficients"/> are modified because we actually put them into reduced row echelon
+        /// form in order to find the steps necessary to do so.
+        /// 
+        /// So make sure that any data associated with the given <paramref name="coefficients"/> is mutated accordingly.
+        /// </remarks>
+        public static IEnumerable<Step> Solve(IList<BitArray> coefficients)
+        {
+            if (coefficients == null)
+                yield break;
+            var numRows = coefficients.Count;
+            if (numRows == 0)
+                yield break;
+            var numColumns = coefficients[0].Length;
+            var kMax = Math.Min(numRows, numColumns);
+
+            // Put matrix into row echelon form
+            for (var k = 0; k < kMax; k++) // O(n^2)
+            {
+                // Find the pivot point
+                var iMax = 0;
+                for (var i = k; i < numRows; i++) // O(n)
+                {
+                    if (!coefficients[i][k])
+                        continue;
+                    iMax = i;
+                    break;
+                }
+                if (!coefficients[iMax][k])
+                    yield break;
+                // Swap rows k and i_max
+                if (iMax != k)
+                {
+                    yield return SwapRows(coefficients, iMax, k);
+                }
+                // XOR pivot with all rows below the pivot
+                for (var i = k + 1; i < numRows; i++) // O(n)
+                {
+                    if (!coefficients[i][k])
+                        continue;
+                    yield return XorRows(coefficients, k, i); // We can just XOR since we're dealing with Galois Fields
+                }
+            }
+
+            // Put the matrix into reduced row echelon form using back substitution
+            for (var k = kMax - 1; k > 0; k--) // O(n^2)
+            {
+                if (!coefficients[k][k])
+                    yield break;
+                // See which other rows need to be XOR'd with this one
+                for (var i = k - 1; i >= 0; i--) // O(n)
+                {
+                    if (!coefficients[i][k])
+                        continue;
+                    yield return XorRows(coefficients, k, i);
+                }
+            }
+
+            // Make sure the top part of the coefficients matrix is the identity matrix and that the bottom part is only zeros
+            for (var row = 0; row < numRows; row++) // O(n^2)
+            {
+                for (var column = 0; column < numColumns; column++) // O(n)
+                {
+                    if ((row == column) ^ coefficients[row][column]) // There should be a coefficient and there's not, or there shouldn't be and there is
+                        yield break;
+                }
+            }
+
+            // Make sure there are at least as many rows as there are columns
+            if (numRows < numColumns)
+                yield break;
+            
+            // If we made it this far, then things have been solved
+            yield return Complete();
+        }
+        
+        private static Step Complete() =>
+            new Step(
+                from: 0,
+                operation: Operation.Complete,
+                to: 0);
+        
+        /// <summary>
+        /// Swaps the coefficients and solutions between the two given rows
+        /// </summary>
+        private static Step SwapRows(
+            IList<BitArray> list,
+            int fromRow,
+            int toRow)
+        {
+            var temp = list[fromRow];
+            list[fromRow] = list[toRow];
+            list[toRow] = temp;
+            return new Step(
+                @from: fromRow,
+                operation: Operation.Swap,
+                to: toRow);
+        }
+
+        /// <summary>
+        /// XOR's the row at <paramref name="fromRow"/> into the row at <paramref name="toRow"/>, for both the coefficients and the solutions
+        /// </summary>
+        private static Step XorRows(
+            IList<BitArray> list,
+            int fromRow,
+            int toRow)
+        {
+            list[toRow].Xor(list[fromRow]);
+            return new Step(
+                @from: fromRow,
+                operation: Operation.Xor,
+                to: toRow);
+        }
+    }
+}
---
+++ Matt.Math.Linear.SolvingTests/GaussianEliminationHelpersClass.cs
@@ -0,0 +1,158 @@
+﻿namespace Matt.Math.Linear.SolvingTests
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Solving;
+
+    [TestClass]
+    public class GaussianEliminationHelpersClass
+    {
+        [TestClass]
+        public class SolveMethod
+        {
+            [TestMethod]
+            public void CanBeCalledWithoutEquations()
+            {
+                // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                GaussianEliminationHelpers.Solve(new List<BitArray>()).ToList();
+            }
+
+            [TestMethod]
+            public void DoesNotSolveUnsolvableSystem()
+            {
+                var coefficients = new List<BitArray>
+                {
+                    new BitArray(new[] { true, false, false })
+                };
+                var solution = GaussianEliminationHelpers.Solve(coefficients).ToList();
+
+                if (solution.Count > 0)
+                    Assert.IsTrue(solution[solution.Count].Operation != Operation.Complete);
+            }
+
+            [TestMethod]
+            public void ProducesStepsWhichResultInIdenticalChangesAndASolution()
+            {
+                var coefficients = new List<BitArray>
+                {
+                    new BitArray(new [] { true, true, true }),
+                    new BitArray(new [] { false, true, true }),
+                    new BitArray(new [] { false, true, false })
+                };
+                var copy = coefficients.Select(x => x.Clone() as BitArray).ToList();
+                
+                var steps = GaussianEliminationHelpers.Solve(coefficients).ToList();
+                
+                // Assert that it has been solved
+                Assert.IsTrue(steps.Count > 0);
+                Assert.AreEqual(Operation.Complete, steps[steps.Count - 1].Operation);
+                Assert.IsTrue(coefficients[0][0]); Assert.IsFalse(coefficients[0][1]); Assert.IsFalse(coefficients[0][2]);
+                Assert.IsFalse(coefficients[1][0]); Assert.IsTrue(coefficients[1][1]); Assert.IsFalse(coefficients[1][2]);
+                Assert.IsFalse(coefficients[2][0]); Assert.IsFalse(coefficients[2][1]); Assert.IsTrue(coefficients[2][2]);
+                
+                // Perform the steps on the copy
+                var mappedOperations = new Dictionary<Operation, Action<int, int, IList<BitArray>>>
+                {
+                    {
+                        Operation.Swap,
+                        (from, to, list) =>
+                        {
+                            var temp = list[to];
+                            list[to] = list[from];
+                            list[from] = temp;
+                        }
+                    },
+                    {
+                        Operation.Xor,
+                        (from, to, list) => list[to].Xor(list[from])
+                    }
+                };
+                foreach (var step in steps)
+                {
+                    if (!mappedOperations.TryGetValue(step.Operation, out var operation))
+                        continue;
+                    operation.Invoke(step.From, step.To, copy);
+                }
+                
+                // Assert that the copy has been solved
+                Assert.IsTrue(copy[0][0]); Assert.IsFalse(copy[0][1]); Assert.IsFalse(copy[0][2]);
+                Assert.IsFalse(copy[1][0]); Assert.IsTrue(copy[1][1]); Assert.IsFalse(copy[1][2]);
+                Assert.IsFalse(copy[2][0]); Assert.IsFalse(copy[2][1]); Assert.IsTrue(copy[2][2]);
+            }
+
+            [TestMethod]
+            public void SolvesAlreadySolvedSystem()
+            {
+                var coefficients = new List<BitArray>
+                {
+                    new BitArray(new[] { true, false, false }),
+                    new BitArray(new[] { false, true, false }),
+                    new BitArray(new[] { false, false, true })
+                };
+                var steps = GaussianEliminationHelpers.Solve(coefficients).ToList();
+                Assert.IsTrue(steps.Count > 0);
+                Assert.AreEqual(Operation.Complete, steps[steps.Count - 1].Operation);
+                Assert.IsTrue(coefficients[0][0]); Assert.IsFalse(coefficients[0][1]); Assert.IsFalse(coefficients[0][2]);
+                Assert.IsFalse(coefficients[1][0]); Assert.IsTrue(coefficients[1][1]); Assert.IsFalse(coefficients[1][2]);
+                Assert.IsFalse(coefficients[2][0]); Assert.IsFalse(coefficients[2][1]); Assert.IsTrue(coefficients[2][2]);
+            }
+
+            [TestMethod]
+            public void SolvesEasilySolvableSystem()
+            {
+                var coefficients = new List<BitArray>
+                {
+                    new BitArray(new[] { false, false, true }),
+                    new BitArray(new[] { false, true, false }),
+                    new BitArray(new[] { true, false, false })
+                };
+                var steps = GaussianEliminationHelpers.Solve(coefficients).ToList();
+                Assert.IsTrue(steps.Count > 0);
+                Assert.AreEqual(Operation.Complete, steps[steps.Count - 1].Operation);
+                Assert.IsTrue(coefficients[0][0]); Assert.IsFalse(coefficients[0][1]); Assert.IsFalse(coefficients[0][2]);
+                Assert.IsFalse(coefficients[1][0]); Assert.IsTrue(coefficients[1][1]); Assert.IsFalse(coefficients[1][2]);
+                Assert.IsFalse(coefficients[2][0]); Assert.IsFalse(coefficients[2][1]); Assert.IsTrue(coefficients[2][2]);
+            }
+
+            [TestMethod]
+            public void SolvesComplicatedSystem()
+            {
+                var coefficients = new List<BitArray>
+                {
+                    new BitArray(new[] {true, false, true}),
+                    new BitArray(new[] {true, true, true}),
+                    new BitArray(new[] {false, false, true})
+                };
+                var steps = GaussianEliminationHelpers.Solve(coefficients).ToList();
+                Assert.IsTrue(steps.Count > 0);
+                Assert.AreEqual(Operation.Complete, steps[steps.Count - 1].Operation);
+                Assert.IsTrue(coefficients[0][0]); Assert.IsFalse(coefficients[0][1]); Assert.IsFalse(coefficients[0][2]);
+                Assert.IsFalse(coefficients[1][0]); Assert.IsTrue(coefficients[1][1]); Assert.IsFalse(coefficients[1][2]);
+                Assert.IsFalse(coefficients[2][0]); Assert.IsFalse(coefficients[2][1]); Assert.IsTrue(coefficients[2][2]);
+            }
+
+            [TestMethod]
+            public void SolvesOverlySolvedSystem()
+            {
+                var coefficients = new List<BitArray>
+                {
+                    new BitArray(new[] { false, false, true }),
+                    new BitArray(new[] { false, true, false }),
+                    new BitArray(new[] { true, false, false }),
+                    new BitArray(new[] { false, false, true }),
+                    new BitArray(new[] { false, true, false }),
+                    new BitArray(new[] { true, false, false })
+                };
+                var steps = GaussianEliminationHelpers.Solve(coefficients).ToList();
+                Assert.IsTrue(steps.Count > 0);
+                Assert.AreEqual(Operation.Complete, steps[steps.Count - 1].Operation);
+                Assert.IsTrue(coefficients[0][0]); Assert.IsFalse(coefficients[0][1]); Assert.IsFalse(coefficients[0][2]);
+                Assert.IsFalse(coefficients[1][0]); Assert.IsTrue(coefficients[1][1]); Assert.IsFalse(coefficients[1][2]);
+                Assert.IsFalse(coefficients[2][0]); Assert.IsFalse(coefficients[2][1]); Assert.IsTrue(coefficients[2][2]);
+            }
+        }
+    }
+}
