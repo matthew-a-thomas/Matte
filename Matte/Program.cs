620049bd8a70c486120584664f4c8b3244e869d8
Matt Thomas <me@matthewathomas.com>
2019-06-19 20:11:29 -0400

    Start console application to demonstrate usage

---
+++ Matte/Program.cs
@@ -0,0 +1,15 @@
+namespace Matte
+{
+    /// <summary>
+    /// Entry point
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        public static void Main()
+        {
+        }
+    }
+}
