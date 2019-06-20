d2e7f86f1fb5e318f2c06d34d4fe32ca7cf985dc
Matt Thomas <me@matthewathomas.com>
2019-06-19 21:33:15 -0400

    Tweak usage demo

--- Matte/Application.cs
+++ Matte/Application.cs
@@ -19,7 +19,7 @@ namespace Matte
             Console.Write("Enter a message: ");
             var message = Console.ReadLine();
             var messageBytes = System.Text.Encoding.UTF8.GetBytes(message);
-            var sliceSize = System.Math.Max(1, messageBytes.Length / 5);
+            var sliceSize = (int)System.Math.Max(1, System.Math.Ceiling(messageBytes.Length / 10.0));
             var slices = _sliceGenerator.Generate(messageBytes, sliceSize);
             foreach (var slice in slices)
             {
@@ -28,13 +28,25 @@ namespace Matte
                     slice
                         .GetCoefficients()
                         .Select(x => x ? "1" : "0"));
-                var data = "0x" + BitConverter
-                    .ToString(slice.GetData().ToArray())
+                var data = slice.GetData().ToArray();
+                var dataAsHexString = "0x" + BitConverter
+                    .ToString(data)
                     .Replace("-", "")
                     .ToLower();
+                var dataAsAscii = new char[data.Length];
+                for (var i = 0; i < data.Length; ++i)
+                {
+                    dataAsAscii[i] = (char) data[i];
+                    if (char.IsControl(dataAsAscii[i]))
+                        dataAsAscii[i] = '?';
+                }
                 Console.Write(coefficients);
                 Console.Write(" -> ");
-                Console.WriteLine(data);
+                Console.Write(dataAsHexString);
+                Console.Write(" \"");
+                Console.Write(dataAsAscii);
+                Console.Write("\"");
+                Console.WriteLine();
                 Thread.Sleep(1000);
             }
         }
